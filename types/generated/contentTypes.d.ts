1832a18ce4f3f1dea5709b6f0a5d29981487e4d5
Dimon <779659252@qq.com>
2025-07-20 06:47:55 +0000

    添加父类别关系到类别模型

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -596,6 +596,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     slug: Schema.Attribute.UID;
     articles: Schema.Attribute.Relation<'oneToMany', 'api::article.article'>;
     description: Schema.Attribute.Text;
+    parent: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
     createdAt: Schema.Attribute.DateTime;
     updatedAt: Schema.Attribute.DateTime;
     publishedAt: Schema.Attribute.DateTime;
